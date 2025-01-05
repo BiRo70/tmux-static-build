ec5a8b0dcfe28ff193186e51757f07b0cc4f2825
Roberto Bigi <roberto.bigi@gmail.com>
2025-01-05 17:33:31 +0000

    moving to stable ncurses 6.3

--- build-static-tmux.sh
+++ build-static-tmux.sh
@@ -15,7 +15,7 @@ COLOR_END="\033[0m"
 PGM="${0##*/}" # Program basename
 
 # Scriptversion
-VERSION=3.5
+VERSION=3.5.1
 
 # How many lines of the error log should be displayed
 LOG_LINES=50
@@ -44,7 +44,7 @@ TMUX_BIN="tmux.${OS}-${ARCH}"
 ######################################
 TMUX_VERSION=3.5
 MUSL_VERSION=1.2.5
-NCURSES_VERSION=6.5
+NCURSES_VERSION=6.3 (stable)
 LIBEVENT_VERSION=2.1.12
 UPX_VERSION=4.2.4
 ######################################
